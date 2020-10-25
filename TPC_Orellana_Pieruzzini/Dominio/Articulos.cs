7b20c8859c8ea7277825394ee4f9cf0560a045f5
carOrellana <carlitos_2355@hotmail.com>
2020-10-25 15:06:54 -0300

    solucion, Domino y clases

---
+++ TPC_Orellana_Pieruzzini/Dominio/Articulos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulos
+    {
+        public int id { get; set; }
+        public string CodigoArticulo { get; set; }
+
+        public string DescricpcionArticulo { get; set; }
+
+        public string Produccion { get; set; }
+
+        public int Stock { get; set; }
+
+        public decimal Precio { get; set; }
+    }
+}
---
+++ TPC_Orellana_Pieruzzini/Dominio/Ventas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Ventas
+    {
+        public int NumeroFactura { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public Clientes Cliente { get; set; }
+
+        public decimal Total { get; set; }
+
+        public string FormaPago { get; set; }
+
+        public int Cantidad { get; set; }
+
+    }
+}
