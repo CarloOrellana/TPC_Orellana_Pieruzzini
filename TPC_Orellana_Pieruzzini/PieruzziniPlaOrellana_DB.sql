a54f9c00f7b961c96ab93a0160206230e1c4e297
ppnt76 <ppnt_76@hotmail.com>
2020-10-31 21:10:17 -0300

    bd actualizada

--- TPC_Orellana_Pieruzzini/PieruzziniPlaOrellana_DB.sql
+++ TPC_Orellana_Pieruzzini/PieruzziniPlaOrellana_DB.sql
@@ -8,6 +8,7 @@ create table Articulos
  Estado bit not null,
  Imagen image null
  )
+ use PieruzziniPlaOrellana_DB
 
 select * from Articulos
  
@@ -39,3 +40,70 @@ select * from ArticuloXMateria
 
  insert into MateriaPrima(Descripcion,Stock)
  values('P12',54)
+
+ create table Rol
+(
+ Id int identity(1,1) primary key,
+ Descripcion char (50)not  null,
+ Estado bit not null
+ )
+
+ create table FormaPago
+(
+ Id int identity(1001,1) primary key,
+ Descripcion char (50)not  null,
+ Estado bit not null
+ )
+
+ create table Tarjetas
+ (
+ Id int identity (1,1) primary key,
+ NumeroTarjeta bigint not null,
+ MesVencimiento int not null,
+ AnioVencimiento int not null,
+ IdFormaPago int foreign key references FormaPago
+ )
+
+ create table DatosPersonales
+ (
+ Id int identity(1,1) primary key,
+ Nombre char(50) not null,
+ Apellido char(50) not null,
+ DNI int not null,
+ Direccion varchar(200) not null,
+ Telefono varchar(15) null,
+ Mail varchar (140) not null,
+ Cuil bigint not null,
+ Estado bit not null
+ )
+
+ create table Usuario
+ (
+ Id int identity(1,1) primary key,
+Contrasenia char(250) not null,
+IdRol int foreign key references Rol(Id)
+)
+
+create table UsuarioXDatos
+(
+IdUsuario int foreign key references Usuario (Id),
+IdDatos int foreign key references DatosPersonales (Id)
+)
+
+create table Ventas
+(
+Id int identity(1,1) primary key,
+IdUsuario int foreign key references Usuario (Id),
+IdFormaPago int foreign key references FormaPago (Id),
+NumeroFactura bigint not null,
+Fecha date not null,
+Total money not null
+)
+
+create table DetalleVentas
+(
+IdVenta int foreign key references Ventas (Id),
+IdArticulo int foreign key references Articulos (Id),
+Cantidad int not null,
+Precio money not null
+)
